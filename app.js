cc5a8587784b351013d12ac4d225a72a7835878a
Cauan-lima <cauanmatheuscalima@gmail.com>
2026-03-19 17:07:54 -0300

    code completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const pokeContainer = document.querySelector("#pokeContainer");
-const pokemonCount = 150
+const pokemonCount = 150;
+
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -15,19 +16,50 @@ const colors = {
     flying: '#F5F5F5',
     fighting: '#E6E0D4',
     normal: '#F5F5F5'
-}
+};
 
-const maintypes = Object.keys(colors);
+const mainTypes = Object.keys(colors);
 
 const fetchPokemons = async () => {
     for (let i = 1; i <= pokemonCount; i++) {
-        await getPokemons(i)
+        await getPokemons(i);
     }
-}
+};
 
-const getPokemons = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon"
-    const resp = await fetch(url)
-    const data = await resp.json()
-    console.log(data.types[0])
-}
+const getPokemons = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const resp = await fetch(url);
+    const data = await resp.json();
+    createPokemonCard(data);
+};
+
+const createPokemonCard = (poke) => {
+    const card = document.createElement('div');
+    card.classList.add("pokemon");
+
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1);
+    const id = poke.id.toString().padStart(3, '0');
+
+    const pokeTypes = poke.types.map(type => type.type.name);
+    const type = mainTypes.find(t => pokeTypes.includes(t));
+    const color = colors[type];
+
+    card.style.backgroundColor = color;
+
+    const pokemonInnerHTML = `
+    <div class="imgContainer">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
+    </div>
+
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>
+    `;
+
+    card.innerHTML = pokemonInnerHTML;
+    pokeContainer.appendChild(card);
+};
+
+fetchPokemons();
